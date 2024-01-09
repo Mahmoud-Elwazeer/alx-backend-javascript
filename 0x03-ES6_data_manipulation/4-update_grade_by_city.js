ff61a36d513c6832cf366331deb4e6e89a4ef859
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-10 01:32:26 +0200

    edit style

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,12 @@
 export default function updateStudentGradeByCity(arr, city, newgrade) {
   if (!Array.isArray(arr)) { return []; }
 
-  const newarr = arr.filter((value) => {
-    return value.location === city;
-  }).map((value) => {
-    const match_grade = newgrade.find(student => student.studentId === value.id);
-    const result = match_grade ? match_grade.grade : 'N/A';
+  const newarr = arr.filter((value) => value.location === city).map((value) => {
+    const matchgrade = newgrade.find((student) => student.studentId === value.id);
+    const result = matchgrade ? matchgrade.grade : 'N/A';
 
-    return {...value, grade:result}
-  })
+    return { ...value, grade: result };
+  });
 
   return (newarr);
 }
