38644e339086894e785d5ec04ddbf610468784ae
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-27 01:21:48 +0200

    type of elements in array

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,14 +1,5 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students)) {
-      throw new TypeError('Students must be a array of Strings');
-    }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -42,6 +33,9 @@ export default class HolbertonCourse {
 
   set students(value) {
     if (!Array.isArray(value)) {
+      throw new TypeError('Students must be a array of Strings');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be a array of Strings');
     }
     this._students = value;
