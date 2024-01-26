a996b33e09dba4da65db21ef3e5284e409140ec8
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-26 23:08:50 +0200

    using map

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,13 +4,13 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((vals) => {
-      vals.forEach((val) => {
-        if (val.status === 'fulfilled') {
-          return { status: val.status, value: val.value };
-        } else {
-          return { status: val.status, value: val.reason };
-        }
-      });
-    });
+    .then((vals) => vals.map((val) => {
+      let out;
+      if (val.status === 'fulfilled') {
+        out = { status: val.status, value: val.value };
+      } else if (val.status === 'rejected') {
+        out = { status: val.status, value: val.reason };
+      }
+      return out;
+    }));
 }
