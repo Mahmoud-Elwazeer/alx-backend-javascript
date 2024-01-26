b5d77015e05f1a04afa8aeed0fec4fe82b6e7fb4
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-26 21:37:33 +0200

    remove async

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
     .then(
       (value) => {
         console.log(`${value[0].body}`, `${value[1].firstName}`, `${value[1].lastName}`);
-      })
-    .catch(
-      () => console.log('Signup system offline')
+      },
     )
-
+    .catch(
+      () => console.log('Signup system offline'),
+    );
 }
