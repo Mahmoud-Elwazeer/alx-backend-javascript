d9c5d2900e6b9b2e109bc6395ac1d7e42b7783de
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-26 21:29:16 +0200

    when recive promise return promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-
-  console.log(photo.body, user.firstName, user.lastName);
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then(
+      (value) => {
+        console.log(`${value[0].body}`, `${value[1].firstName}`, `${value[1].lastName}`);
+      },
+      () => {
+        console.log('Signup system offline');
+      },
+    );
 }
