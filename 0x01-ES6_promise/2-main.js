505f2e097b3d816abfef1ce788ba2b1d74177c55
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-26 17:10:48 +0200

    test

--- 0x01-ES6_promise/2-main.js
+++ 0x01-ES6_promise/2-main.js
@@ -1,4 +1,11 @@
 import handleResponseFromAPI from "./2-then";
 
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
+test("handleResponseFromAPI return the right response on resolve", () => {
+  const promise = Promise.resolve();
+
+  const successResponse = handleResponseFromAPI(promise);
+  return expect(successResponse).resolves.toEqual({
+    body: 'success',
+    status: 200
+  });
+});
