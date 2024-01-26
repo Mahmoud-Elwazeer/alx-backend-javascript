21484ac55164737f40cafa139ca8db426e73c971
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-26 17:00:49 +0200

    test

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then((value) => {
-    {
-      status: 200
-      body: 'success'
-    };
-  });
-  promise.catch((err) => { new Error('') });
-  console.log("Got a response from the API");
+  promise.then(
+    (value) => {
+      return { status: 200, body: 'success' };
+    },
+    (error) => {
+      return new Error();
+    }
+  );
+  console.log('Got a response from the API');
 }
