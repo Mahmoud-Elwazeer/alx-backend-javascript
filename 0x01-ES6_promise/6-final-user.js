c873c8295bf4e8752c8b06a78f38a9e973e59303
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-26 22:40:07 +0200

    test

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,13 +3,14 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((val) => [
-      { status: 'fulfilled', value: val[0] },
-      { status: 'fulfilled', value: val[1] },
-    ],
-    (err) => [
-      { status: 'rejected', value: err[0] },
-      { status: 'rejected', value: err[1] },
-    ]);
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((vals) => {
+      vals.forEach((val) => {
+        if (val.status === 'fulfilled') {
+          console.log({ status: val.status, value: val.value });
+        } else if (val.status === 'rejected') {
+          console.log({ status: val.status, value: val.reason });
+        }
+      });
+    });
 }
