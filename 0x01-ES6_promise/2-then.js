4edf9cbb5fc7ed410fa5abaf311dc86c8b00c628
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-26 17:27:58 +0200

    edit style

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,13 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then(
-    (value) => {
+    () => {
       console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
       };
     },
-    (error) => {
+    () => {
       console.log('Got a response from the API');
       return new Error();
     },
