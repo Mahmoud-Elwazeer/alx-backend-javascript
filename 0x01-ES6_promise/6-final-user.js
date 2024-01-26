17f8a8a8a5e2acc1abf40cec91f5762670483bf6
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-26 22:31:38 +0200

    test

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,11 +5,11 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((val) => [
-      { status: 'Fulfilled', value: val[0] },
-      { status: 'Fulfilled', value: val[1] },
+      { status: 'fulfilled', value: val[0] },
+      { status: 'fulfilled', value: val[1] },
     ],
     (err) => [
-      { status: 'Rejected', value: err[0] },
-      { status: 'Rejected', value: err[1] },
+      { status: 'rejected', value: err[0] },
+      { status: 'rejected', value: err[1] },
     ]);
 }
