1565c017f732a1c1e73d3c63b4689f65a847b6ea
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-26 21:41:35 +0200

    test another same sol

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,25 @@
+// import { uploadPhoto, createUser } from './utils';
+
+// export default function handleProfileSignup() {
+//   return Promise
+//     .all([uploadPhoto(), createUser()])
+//     .then(
+//       (value) => {
+//         console.log(`${value[0].body}`, `${value[1].firstName}`, `${value[1].lastName}`);
+//       },
+//     )
+//     .catch(
+//       () => console.log('Signup system offline'),
+//     );
+// }
+
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then(
-      (value) => {
-        console.log(`${value[0].body}`, `${value[1].firstName}`, `${value[1].lastName}`);
-      },
-    )
-    .catch(
-      () => console.log('Signup system offline'),
-    );
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
