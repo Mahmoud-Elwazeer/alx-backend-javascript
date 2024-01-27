ba71d2fec5b5177b48c1769c7ac94c14514da8a6
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-27 02:31:08 +0200

    check extends have specific method or not

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Sqft must be a Number');
+    }
+    this._sqft = value;
+  }
+}
