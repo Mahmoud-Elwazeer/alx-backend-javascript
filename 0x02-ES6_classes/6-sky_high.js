b8a6f970bb26e18aacb9a05895fe23baab9458bd
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-27 02:53:45 +0200

    extend class from another class

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,9 +1,9 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor (sqft, floors) {
-    super(sqft)
-    this.floors = floors
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
   }
 
   get floors() {
