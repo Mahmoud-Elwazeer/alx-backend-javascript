6815c14520f76e0b58460b3fe74b3fe41f82987a
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-27 23:03:42 +0200

    creat server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,12 @@
 const http = require('http');
 
+const hostname = 'localhost';
+const port = 1245;
+
 const app = http.createServer((req, res) => {
-  res.write('Hello Holberton School!');
-  res.end();
-}).listen(1245);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname);
