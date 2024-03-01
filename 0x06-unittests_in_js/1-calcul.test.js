b368ab2bcc5c814749b158ba48ff914700dd62e5
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-03-01 02:49:30 +0200

    update function to receive type

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,20 +1,20 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul.js');
+const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', () => {
   it('should be around the rounded part', () => {
-    assert.equal(calculateNumber(1, 2), 3);
+    assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
   });
 
   it('should be around the rounded part', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
   });
 
   it('should be around the rounded part', () => {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
   });
 
   it('should be around the rounded part', () => {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
   });
 });
