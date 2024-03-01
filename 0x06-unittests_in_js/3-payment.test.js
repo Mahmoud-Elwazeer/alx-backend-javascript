08d339c67fdd7baa2bcb84aac24d4f0ef729cfcf
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-03-01 03:33:22 +0200

    sinon"

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sendPaymentRequestToApi = require('./3-payment.js');
+const Utils = require('./utils.js')
+const chai = require("chai");
+const expect = chai.expect;
+const sinon = require("sinon");
+
+describe('sendPaymentRequestToApi', () => {
+    it('should be around the rounded part', () => {
+      const send_test = sinon.spy(Utils)
+
+      sendPaymentRequestToApi(100, 20);
+      expect(send_test.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+      expect(send_test.calculateNumber.callCount).to.be.equal(1);
+      send_test.calculateNumber.restore();
+    });
+});
