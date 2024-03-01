4c62ffa438d6a971198054cfbc41eb35a5ee63e9
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-03-01 03:56:23 +0200

    using before and after each

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,30 @@
+const sendPaymentRequestToApi = require('./3-payment.js');
+const Utils = require('./utils.js');
+const chai = require("chai");
+const expect = chai.expect;
+const sinon = require("sinon");
+
+describe('sendPaymentRequestToApi', () => {
+    let spy;
+
+    beforeEach(() => {
+      spy = sinon.spy(console);
+    })
+
+    afterEach(() => {
+      spy.restore()
+    })
+    it('sendPaymentRequestToAPI with 100, and 20', () => {
+
+      sendPaymentRequestToApi(100, 20);
+      expect(spy.log.calledWith('The total is: 120')).to.be.true;
+      expect(spy.log.callCount).to.be.equal(1);
+    });
+
+    it('sendPaymentRequestToAPI with 10, and 10', () => {
+
+      sendPaymentRequestToApi(10, 10);
+      expect(spy.log.calledWith('The total is: 20')).to.be.true;
+      expect(spy.log.callCount).to.be.equal(1);
+    });
+});
