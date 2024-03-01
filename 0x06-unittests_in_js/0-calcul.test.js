105a34b846ab782a8ca83e445a4b7aeba9b294fc
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-03-01 02:51:01 +0200

    #

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,7 +3,7 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
   it('should be around the rounded part', () => {
-    assert.equal(calculateNumber(1, 2), 3);
+    assert.equal(calculateNumber(1, 3), 4);
   });
 
   it('should be around the rounded part', () => {
