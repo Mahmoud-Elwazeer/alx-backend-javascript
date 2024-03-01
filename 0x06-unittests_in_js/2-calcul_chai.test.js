2139371da3279073eb9fc8f6fd767c5e9712ea09
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-03-01 03:01:37 +0200

    chai test

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require('./1-calcul.js');
+
+describe('calculateNumber', () => {
+  it('should be around the rounded part', () => {
+    const result = calculateNumber('SUM', 1.4, 4.5);
+    expect(result).to.equal(6);
+  });
+
+  it('should be around the rounded part', () => {
+    const result = calculateNumber('SUBTRACT', 1.4, 4.5);
+    expect(result).to.equal(-4);
+  });
+
+  it('should be around the rounded part', () => {
+    const result = calculateNumber('DIVIDE', 1.4, 4.5);
+    expect(result).to.equal(0.2);
+  });
+
+  it('should be around the rounded part', () => {
+    const result = calculateNumber('DIVIDE', 1.4, 0);
+    expect(result).to.equal('Error');
+  });
+});
