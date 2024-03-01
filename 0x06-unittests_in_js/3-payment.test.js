c58db68dd8979bf072cf725f0315d7c06dbb79e1
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-03-01 03:45:51 +0200

    use stub

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,12 +1,12 @@
 const sendPaymentRequestToApi = require('./3-payment.js');
-const Utils = require('./utils.js')
+const Utils = require('./utils.js');
 const chai = require("chai");
 const expect = chai.expect;
 const sinon = require("sinon");
 
 describe('sendPaymentRequestToApi', () => {
     it('should be around the rounded part', () => {
-      const send_test = sinon.spy(Utils)
+      const send_test = sinon.spy(Utils);
 
       sendPaymentRequestToApi(100, 20);
       expect(send_test.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,24 @@
+const sendPaymentRequestToApi = require('./4-payment.js');
+const Utils = require('./utils.js');
+const chai = require("chai");
+const expect = chai.expect;
+const sinon = require("sinon");
+
+describe('sendPaymentRequestToApi', () => {
+    it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+      const spy = sinon.spy(console);
+      const stub = sinon.stub(Utils, 'calculateNumber');
+
+      stub.returns(10);
+
+
+      sendPaymentRequestToApi(100, 20);
+      expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+      expect(stub.callCount).to.be.equal(1);
+      expect(spy.log.calledWith('The total is: 10')).to.be.true;
+      expect(spy.log.callCount).to.be.equal(1);
+
+      spy.restore();
+      stub.log.restore();
+    });
+});
