9cbd306800c2cb90302f8f98b0dd29659098af56
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-03-01 18:26:41 +0200

    #

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,11 +1,10 @@
 const chai = require("chai");
 const expect = chai.expect;
-chai.use(require("chai-as-promised"));
 const getPaymentTokenFromAPI = './6-payment_token.js';
 
 describe(('getPaymentTokenFromAPI', () => {
   it('Async tests with done', (done) => {
-    return getPaymentTokenFromAPI(true)
+    getPaymentTokenFromAPI(true)
       .then((res) => {
         expect(res).to.deep.equal({data: 'Successful response from the API' });
         done();
